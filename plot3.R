ddc90c8e8b974cdea9d37c481c1118ebad9116b6
tom43214 <anon>
2014-12-07 14:45:32 -0800

    Added more comments, modified png rendering

--- plot3.R
+++ plot3.R
@@ -1,17 +1,24 @@
 plot3 <- function() {
 
-      ##Read in data file assuming it is in the current directory
+      ##Set classes for columns of data frame to read in
       classes <- c("character", "character", rep("numeric", 7))
+      
+      ##Read in data file
+      ##Data is assumed to be in working directory
       dat <- read.table("household_power_consumption.txt", sep = ";",
                         header=TRUE, na.strings = "?", colClasses = classes)
       
+      ##Subset data file for dates requested in assignment
       dat <- subset(dat, Date == "1/2/2007" | Date == "2/2/2007")
       
+      ##Add column to data frame combining date and time in POSIXlt format
       dat <- within(dat, datetime <- strptime(paste(Date, Time), 
                                               "%d/%m/%Y %H:%M:%S"))
       
-      ##Open PNG file device for saving plot
-      png(filename = "plot3.png", width = 480, height = 480)
+      
+      ##Open PNG file device with appropriate arguments for saving plot
+      png(filename = "plot3.png", width = 480, height = 480,
+          bg = "transparent", type = "cairo")
       
       ##Set up plot with Sub_metering_1 data
       with(dat, plot(datetime, Sub_metering_1, type = "l",
--- plot4.R
+++ plot4.R
@@ -1,20 +1,26 @@
 plot4 <- function() {
       
-      ##Read in data file assuming it is in the current directory
+      ##Set classes for columns of data frame to read in
       classes <- c("character", "character", rep("numeric", 7))
+      
+      ##Read in data file
+      ##Data is assumed to be in working directory
       dat <- read.table("household_power_consumption.txt", sep = ";",
                         header=TRUE, na.strings = "?", colClasses = classes)
       
+      ##Subset data file for dates requested in assignment
       dat <- subset(dat, Date == "1/2/2007" | Date == "2/2/2007")
       
+      ##Add column to data frame combining date and time in POSIXlt format
       dat <- within(dat, datetime <- strptime(paste(Date, Time), 
                                               "%d/%m/%Y %H:%M:%S"))
       
       
-      ##Open PNG file device for saving plot
-      png(filename = "plot4.png", width = 480, height = 480)
+      ##Open PNG file device with appropriate arguments for saving plot
+      png(filename = "plot4.png", width = 480, height = 480,
+          bg = "transparent", type = "cairo")
       
-      ##Set up plots in 2 by 2 rows with 15 point font for to match plot sizes
+      ##Set up plots in 2 by 2 rows with 11 point font to match plot sizes
       par(mfrow = c(2, 2), ps = 11)
       
       ##Set up plot 1
